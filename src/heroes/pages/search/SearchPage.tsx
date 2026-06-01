8f02dfdb2862fd866d4fadf037238f90ff36058c
Carlos Ramos <carlosramosdev@protonmail.com>
2026-06-01 21:45:40 +0200

    rafactor

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,9 +1,13 @@
 import { CustomJumbotron } from '@/components/custom';
+import { HeroStats } from '@/heroes/components';
+import { SearchControls } from './components/SearchControls';
 
 export const SearchPage = () => {
   return (
     <>
       <CustomJumbotron title="Búsqueda de superhéroes y villanos" />
+      <HeroStats />
+      <SearchControls />
     </>
   );
 };
