f62c94a5f1af0bd027ff2a27a68766f90c738d0c
Carlos Ramos <carlosramosdev@protonmail.com>
2026-06-01 17:51:12 +0200

    configuredshadcn/ui

---
+++ src/HeroesApp.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@components/ui';
+
+export const HeroesApp = () => {
+  return (
+    <>
+      <h1>Heroes App!</h1>
+      <Button>Esto es un botón</Button>
+    </>
+  );
+};
