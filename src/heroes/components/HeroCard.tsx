a75c9b9072c6a0780c59f97be68cd06979b97adc
Carlos Ramos <carlosramosdev@protonmail.com>
2026-06-02 17:35:47 +0200

    implemented navigation to heroes

--- src/heroes/components/HeroCard.tsx
+++ src/heroes/components/HeroCard.tsx
@@ -9,6 +9,7 @@ import {
   Progress,
 } from '@/components/ui';
 import type { Hero } from '../types';
+import { useNavigate } from 'react-router';
 
 interface Props {
   hero: Hero;
@@ -16,6 +17,12 @@ interface Props {
 }
 
 export const HeroCard = ({ hero, isFavourite = false }: Props) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/heroes/${hero.slug}`);
+  };
+
   const ownerColor = (): string => {
     if (hero.universe === 'DC') return 'bg-blue-600';
     if (hero.universe === 'Marvel') return 'bg-red-600';
@@ -32,11 +39,12 @@ export const HeroCard = ({ hero, isFavourite = false }: Props) => {
 
   return (
     <Card className="group overflow-hidden hover:shadow-xl transition-all duration-300 hover:-translate-y-1 bg-linear-to-br from-white to-gray-50">
-      <div className="relative h-64 overflow-hidden">
+      <div className="relative h-64">
         <img
           src={hero.image}
           alt={hero.slug}
-          className="object-cover transition-all duration-500 group-hover:scale-110"
+          className="object-cover transition-all duration-500 group-hover:scale-110 absolute -top-7.5 w-full h-102.5"
+          onClick={handleClick}
         />
 
         {/* Status indicator */}
@@ -80,7 +88,7 @@ export const HeroCard = ({ hero, isFavourite = false }: Props) => {
         </Button>
       </div>
 
-      <CardHeader className="pb-3">
+      <CardHeader className="py-3 z-10 bg-gray-100/50 backdrop-blur-sm relative top-1 group-hover:-top-2.5 transition-all duration-300">
         <div className="flex justify-between items-start">
           <div className="space-y-1">
             <h3 className="font-bold text-lg leading-tight">{hero.alias}</h3>
@@ -149,7 +157,7 @@ export const HeroCard = ({ hero, isFavourite = false }: Props) => {
           <h4 className="font-medium text-sm">Powers:</h4>
           <div className="flex flex-wrap gap-1">
             {hero.powers.slice(0, 3).map((power) => (
-              <Badge variant="outline" className="text-xs">
+              <Badge variant="outline" className="text-xs" key={power}>
                 {power}
               </Badge>
             ))}
--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: 'heroes/1',
+        path: 'heroes/:idSlug',
         element: <HeroPage />,
       },
       {
