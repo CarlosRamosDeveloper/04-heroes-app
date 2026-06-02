42dfc031f20ac15e59c0c7f33b714c654f2b444f
Carlos Ramos <carlosramosdev@protonmail.com>
2026-06-02 12:23:54 +0200

    added environment variables

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,5 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-interface ImportMetaEnv {}
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
