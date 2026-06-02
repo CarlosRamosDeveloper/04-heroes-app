ac6abf0936efa95e59b66f5fc5a2ea6d82c8cf2d
Carlos Ramos <carlosramosdev@protonmail.com>
2026-06-02 13:13:31 +0200

    implemented tanstack & devtools

--- src/HeroesApp.tsx
+++ src/HeroesApp.tsx
@@ -1,10 +1,19 @@
 import { RouterProvider } from 'react-router';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { router } from './router/app.router';
+
+const queryClient = new QueryClient();
 
 export const HeroesApp = () => {
   return (
     <>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </>
   );
 };
