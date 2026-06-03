6dd05e93300b9f40e813eab1a5c5b1ce7b2e1a68
Carlos Ramos <carlosramosdev@protonmail.com>
2026-06-03 11:03:24 +0200

    pagination now returns content correctly

--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -23,7 +23,7 @@ export default function HomePage() {
   };
 
   const { data: heroesResponse } = useQuery({
-    queryKey: ['heroes'],
+    queryKey: ['heroes', { page, limit }],
     queryFn: () => getHeroesByPageAction(+page, +limit),
     staleTime: 1000 * 60 * 5,
   });
